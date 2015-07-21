659c3b6d2bfa638f3aa8fdb1b37e011deb1472c8
kiran-mk <kiran.malleshappa@qualitest.co.uk>
2015-07-21 17:55:21 +0530

    resolved #25

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              "java"
 maintainer        "Agile Orbit"
 maintainer_email  "info@agileorbit.com"
-license           "Apache 2.0"zxcv
+license           "Apache 2.0"zxcvasfd
 description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.32.1"
